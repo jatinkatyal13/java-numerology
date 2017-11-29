39650592dd0213b139eb51a125ce468efa26fe64
Jatin <jatin.katyal13@gmail.com>
2017-11-29 15:13:00 +0530

    Initial

---
+++ src/person.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Amit
+ */
+public class person {
+    static int id;
+    static String name;
+    static int dd,mm,yy;
+    static int talent_num,path1,path2,path3,path4;
+    static int  destiny_num, heart_num,person_num,ultimate_num;
+   
+}
